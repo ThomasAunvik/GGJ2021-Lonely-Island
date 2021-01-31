f457cd44277fbb82dad59702e8ae4d69affa45f0
Don <donjete.haziri@ntnu.no>
2021-01-31 08:17:27 +0100

    All Fox assets

---
+++ Assets/Don/Scripts/BearAnimationScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxAnimationScript : MonoBehaviour
+{
+    public Animator animator;
+
+    /* The objects we need scripts from */
+    // GameObject object;
+
+    /* The scripts we need variables from */
+    // ScriptName script; 
+
+
+    void Start()
+    {
+        // object = GameObject.FindWithTag("Tag");
+
+        // script = object.GetComponent<ScriptName>();
+    }
+
+
+    void Update()
+    {
+        // animator.SetBool("InHubWorld", script.ABool);
+        // animator.SetBool("Dialog", script.ABool);
+    }
+}
